b0d830406a38979c7b5b7fecfae17bbe256da9c9
bluelovers <codelovers@users.sourceforge.net>
2020-01-08 07:28:14 +0800

    publish new version

--- index.ts
+++ index.ts
@@ -225,6 +225,12 @@ export function _createYarnWorkspaces(targetPath: string, options: IOptions = {}
 
 		lerna = {
 			"packages": packages,
+			"command": {
+				"publish": {
+					"ignoreChanges": ["node_modules"],
+					"message": "chore(release): publish"
+				}
+			},
 			"npmClient": "yarn",
 			"useWorkspaces": true,
 			"version": "independent",
